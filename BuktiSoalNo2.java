c86ed9b36432f39610c572cf2909e22eee3fdb54
muhammadhanif17 <hanifmuhammad1309@gmail.com>
2024-09-09 17:30:49 +0700

    p

---
+++ BuktiSoalNo2.java
@@ -0,0 +1,9 @@
+public class BuktiSoalNo2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Nama : Muhammad Hanif");
+        System.out.println("NIM  : 244107020118 ");
+        
+    }
+}
